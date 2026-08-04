8571b43c0493742f8cf6ba9a390345d31248d82f
QuantumBell <basilhamad42@gmail.com>
2026-08-03 22:16:17 -0700

    Update InspectGrid.m

--- image_processing_code/screw and grid/InspectGrid.m
+++ image_processing_code/screw and grid/InspectGrid.m
@@ -1,69 +1,64 @@
-function [metrics, redOverlay] = inspectGrid(img, defectType)
+function [metrics, red_overlay] = inspectGrid(img, defectType)
 
 
-    se = strel('disk', 7);
-    closedse = strel('disk', 3);
-    defectType = lower(string(defectType));
+se = strel('disk', 7);
+closedse = strel('disk', 3);
 
-    rawImg = imread(img);
+defectType = lower(string(defectType));
 
-    binarizedimage = imbinarize(rawImg);
 
-    BW = imdilate(binarizedimage, strel('disk',1));
-    openimage = imopen(BW, se);
-    filledimage = imfill(openimage, "holes");
-    closedimage = imclose(filledimage,closedse);
-    finalImg = imfill(closedimage, "holes");
-    holes = imclearborder(finalImg);
-    CC = bwconncomp(holes);
-    stats = regionprops("table", CC, "Area", "BoundingBox");
-    area = stats.Area;
+
+
+binarizedimage = imbinarize(img);
+
+BW = imdilate(binarizedimage, strel('disk',1));
+openimage = imopen(BW, se);
+filledimage = imfill(openimage, "holes");
+closedimage = imclose(filledimage,closedse);
+finalImg = imfill(closedimage, "holes");
+holes = imclearborder(finalImg);
+CC = bwconncomp(holes);
+stats = regionprops("table", CC, "Area", "BoundingBox");
+area = stats.Area;
 
 switch defectType
 
     case{"broken"}
 
-    medArea = median(area);
-    scale = 1.4826 * mad(area, 1);
+        medArea = median(area);
+        scale = 1.4826 * mad(area, 1);
 
-    upperCutoff = medArea + 3*scale;
-    selection = area > upperCutoff;
-    BW2 = cc2bw (CC, ObjectsToKeep = selection);
-
-    redChannel = uint8(BW2) * 255; 
-
-    greenChannel = zeros(size(BW2), 'uint8');
-    blueChannel  = zeros(size(BW2), 'uint8');
-
-    redOverlay = cat(3, redChannel, greenChannel, blueChannel);
-
-    metricMask = bwconncomp(BW2);
-
-    metrics = metricMask.NumObjects;
-
+        upperCutoff = medArea + 3*scale;
+        selection = area > upperCutoff;
+        BW2 = cc2bw (CC, ObjectsToKeep = selection);
+        red_overlay = imoverlay(img, BW2, "red");
 
     case{"glue"}
 
-     medArea = median(area);
-     stdArea = std(area);
-     areaCutoff = medArea - (2.5 * stdArea);
-     selection = (area < areaCutoff);
-     BW2 = cc2bw (CC, ObjectsToKeep = selection);
-
-     redChannel = uint8(BW2) * 255; 
-
-     greenChannel = zeros(size(BW2), 'uint8');
-     blueChannel  = zeros(size(BW2), 'uint8');
-
-     redOverlay = cat(3, redChannel, greenChannel, blueChannel);
-  
-     metricMask = bwconncomp(BW2);
-
-     metrics = metricMask.NumObjects;
+        medArea = median(area);
+        stdArea = std(area);
+        areaCutoff = medArea - (2.5 * stdArea);
+        selection = (area < areaCutoff);
+        BW2 = cc2bw (CC, ObjectsToKeep = selection);
+        red_overlay = imoverlay(img, BW2, "red");
 
 
+    otherwise
+        warning("Unrecognized defect type: '%s'. Returning empty defect mask.", defectType);
 
 end
 
+connectedRegions = bwconncomp(BW2);
+metrics.numComponents = connectedRegions.NumObjects;
 
+areaStats = regionprops(BW2, "Area");
 
+if isempty(areaStats)
+    metrics.maxArea = 0;
+else
+    metrics.maxArea = max([areaStats.Area]);
+end
+
+metrics.areaRatio = nnz(BW2) / numel(BW2);
+
+end
