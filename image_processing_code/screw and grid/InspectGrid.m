67974c40c9e8691e2630971da2d10dc98bf56c26
QuantumBell <basilhamad42@gmail.com>
2026-08-05 17:23:19 -0700

    Update InspectGrid.m
    added 3 more cases. Bent, metal_contamination, and thread.

--- image_processing_code/screw and grid/InspectGrid.m
+++ image_processing_code/screw and grid/InspectGrid.m
@@ -1,29 +1,24 @@
+
+
 function [metrics, red_overlay] = inspectGrid(img, defectType)
-
-
-se = strel('disk', 7);
-closedse = strel('disk', 3);
-
 defectType = lower(string(defectType));
-
-
-
-
-binarizedimage = imbinarize(img);
-
-BW = imdilate(binarizedimage, strel('disk',1));
-openimage = imopen(BW, se);
-filledimage = imfill(openimage, "holes");
-closedimage = imclose(filledimage,closedse);
-finalImg = imfill(closedimage, "holes");
-holes = imclearborder(finalImg);
-CC = bwconncomp(holes);
-stats = regionprops("table", CC, "Area", "BoundingBox");
-area = stats.Area;
 
 switch defectType
 
     case{"broken"}
+        se = strel('disk', 7);
+        closedse = strel('disk', 3);
+        binarizedimage = imbinarize(img);
+
+        BW = imdilate(binarizedimage, strel('disk',1));
+        openimage = imopen(BW, se);
+        filledimage = imfill(openimage, "holes");
+        closedimage = imclose(filledimage,closedse);
+        finalImg = imfill(closedimage, "holes");
+        holes = imclearborder(finalImg);
+        CC = bwconncomp(holes);
+        stats = regionprops("table", CC, "Area", "BoundingBox");
+        area = stats.Area;
 
         medArea = median(area);
         scale = 1.4826 * mad(area, 1);
@@ -35,12 +30,88 @@ switch defectType
 
     case{"glue"}
 
+        se = strel('disk', 10);
+        closedse = strel('disk', 5);
+
+        binarizedimage = imbinarize(img);
+
+        BW = imdilate(binarizedimage, strel('disk',1));
+        openimage = imopen(BW, se);
+        filledimage = imfill(openimage, "holes");
+        closedimage = imclose(filledimage,closedse);
+        finalImg = imfill(closedimage, "holes");
+        holes = imclearborder(finalImg);
+        CC = bwconncomp(holes);
+        stats = regionprops("table", CC, "Area", "BoundingBox");
+        area = stats.Area;
+
+        medArea = median(area);
+        stdArea = std(area);
+        areaCutoff = medArea - (1.5 * stdArea);
+        selection = (area < areaCutoff);
+        BW2 = cc2bw (CC, ObjectsToKeep = selection);
+        red_overlay = imoverlay(img, BW2, "red");
+
+    case{"metal_contamination"}
+        
+        se = strel('disk', 7);
+        closedse = strel('disk', 3);
+
+        binarizedimage = imbinarize(img);
+
+        BW = imdilate(binarizedimage, strel('disk',1));
+        openimage = imopen(BW, se);
+        filledimage = imfill(openimage, "holes");
+        closedimage = imclose(filledimage,closedse);
+        finalImg = imfill(closedimage, "holes");
+        holes = imclearborder(finalImg);
+        CC = bwconncomp(holes);
+        stats = regionprops("table", CC, "Area", "BoundingBox");
+        area = stats.Area;
+
         medArea = median(area);
         stdArea = std(area);
         areaCutoff = medArea - (2.5 * stdArea);
         selection = (area < areaCutoff);
         BW2 = cc2bw (CC, ObjectsToKeep = selection);
         red_overlay = imoverlay(img, BW2, "red");
+
+
+    case{"thread"}
+
+        se1 = strel('disk', 20);
+        se2 = strel('disk', 18);
+        finalimg = imbinarize(img);
+        img_inv = ~finalimg;
+        finalimg = imopen(img_inv , se1);
+        BW2 = imclose(finalimg, se2);
+
+
+        red_overlay = imoverlay(img, finalimg, "red");
+
+    case{"bent"}
+        se = strel('disk', 10);
+        closedse = strel('disk', 5);
+
+        binarizedimage = imbinarize(img);
+
+        BW = imdilate(binarizedimage, strel('disk',1));
+        openimage = imopen(BW, se);
+        filledimage = imfill(openimage, "holes");
+        closedimage = imclose(filledimage,closedse);
+        finalImg = imfill(closedimage, "holes");
+        holes = imclearborder(finalImg);
+        CC = bwconncomp(holes);
+        stats = regionprops("table", CC, "Area", "BoundingBox");
+        area = stats.Area;
+
+        medArea = median(area);
+        stdArea = std(area);
+        areaCutoff = medArea + (1.8 * stdArea);
+        selection = (area > areaCutoff);
+        BW2 = cc2bw (CC, ObjectsToKeep = selection);
+        red_overlay = imoverlay(img, BW2, "red");
+
 
 
     otherwise
